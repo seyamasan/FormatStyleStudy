a49956623c7732496fcf7488f47bb21ad583862c
seyamasan <kst_apr_24@icloud.com>
2025-10-01 20:46:20 +0900

    Initial Commit

---
+++ FormatStyleStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormatStyleStudy
+//
+//  Created by 瀬山皐太 on 2025/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormatStyleStudy/FormatStyleStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatStyleStudyApp.swift
+//  FormatStyleStudy
+//
+//  Created by 瀬山皐太 on 2025/10/01.
+//
+
+import SwiftUI
+
+@main
+struct FormatStyleStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
