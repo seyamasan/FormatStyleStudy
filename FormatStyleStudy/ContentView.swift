61ad7b5cb380dc82671d3df464f4aa4fb59c40e1
seyamasan <kst_apr_24@icloud.com>
2025-10-01 21:23:27 +0900

    feat: シンプルな日付フォーマットを実装

--- FormatStyleStudy/ContentView.swift
+++ FormatStyleStudy/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let simpleDateFormatTitle = "シンプルな日付フォーマット"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("日付") {
+                    let now = Date()
+                    
+                    NavigationLink {
+                        SimpleDateFormatView(title: simpleDateFormatTitle, date: now)
+                    } label: {
+                        Text(simpleDateFormatTitle)
+                    }
+                }
+            }
+            .navigationTitle("FormatStyle")
         }
-        .padding()
     }
 }
 
---
+++ FormatStyleStudy/SimpleDateFormatView.swift
@@ -0,0 +1,24 @@
+//
+//  DateFormatView.swift
+//  FormatStyleStudy
+//
+//  Created by 瀬山皐太 on 2025/10/01.
+//
+
+import SwiftUI
+
+struct SimpleDateFormatView: View {
+    var title: String
+    var date: Date
+    
+    var body: some View {
+        VStack {
+            Text(date.formatted())
+        }
+        .navigationTitle(title)
+    }
+}
+
+#Preview {
+    ContentView()
+}
