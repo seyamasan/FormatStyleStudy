836cd160187ff835be02afa71fb863706e103ff7
seyamasan <kst_apr_24@icloud.com>
2025-10-02 20:52:59 +0900

    refactor: JapaneseCalendarDateFormatViewのUIを改善

--- FormatStyleStudy/JapaneseCalendarDateFormatView.swift
+++ FormatStyleStudy/JapaneseCalendarDateFormatView.swift
@@ -18,14 +18,29 @@ struct JapaneseCalendarDateFormatView: View {
         = "\(era: .narrow)\(year: .twoDigits).\(month: .twoDigits).\(day: .twoDigits)"
     
     var body: some View {
-        VStack {
-            Text(date.formatted(.verbatim(
-                jisX0301Format,
-                locale: Locale(languageCode: .japanese, languageRegion: .japan),
-                timeZone: TimeZone(identifier: "Asia/Tokyo")!,
-                calendar: Calendar(identifier: .japanese)
-            )))
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                Text("元のDate")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text("\(date)")
+            }
+
+            Divider()
+
+            HStack {
+                Text("フォーマット後")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text(date.formatted(.verbatim(
+                    jisX0301Format,
+                    locale: Locale(languageCode: .japanese, languageRegion: .japan),
+                    timeZone: TimeZone(identifier: "Asia/Tokyo")!,
+                    calendar: Calendar(identifier: .japanese)
+                )))
+            }
         }
+        .padding()
         .navigationTitle(title)
     }
 }
